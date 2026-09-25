eef637259ae8c247bd77db8eb5cc492a759c709d
Modular <filip.bednarglados@gmail.com>
2026-09-25 04:10:56 +0200

    Final review added

--- ib111_final.sh
+++ ib111_final.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+failed=0
+
 echo "Spouštím test SANITY na přípravách ve složce $(pwd)"
 echo "----------"
 for FILE in *; do
@@ -8,6 +10,7 @@ for FILE in *; do
 		SECONDS=0
 		if ! /home/modular/projects/archScripts/ib111_run.sh $FILE; then
 			echo -e "\e[31mSANITY FAIL\e[0m"
+			failed=1
 		else
 			echo -e "\e[92mSANITY SUCCESS\e[0m"
 		fi
@@ -16,3 +19,9 @@ for FILE in *; do
 		echo "----------"
 	fi
 done
+
+if [[ $failed -eq 1 ]]; then
+	echo -e "\e[31mNěkteré přípravy selhaly.\e[0m"
+else
+	echo -e "\e[92mVšechny přípravy uspěly.\e[0m"
+fi
